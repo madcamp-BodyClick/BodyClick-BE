6fdbca2ab439f6aa5c9e4bd2527531136e7aa6af
FaceWithNull <dooopro00@gmail.com>
2026-01-18 15:18:23 +0900

    implement oauth

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>BodyClick 백엔드 서버에 오신 것을 환영합니다!</h1>
+      <p>현재 로그인이 완료된 상태입니다.</p>
+    </div>
+  );
+}
